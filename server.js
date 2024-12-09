6ebecc37f7645b43646003671669d242c2f6aa9e
Vivek-4321 <vivekvenugopal514@gmail.com>
2024-12-09 13:13:54 +0530

    Minor Bug Fixes...

--- server.js
+++ server.js
@@ -46,7 +46,7 @@ wss.on('connection', (ws) => {
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-const PORT = process.env.PORT || 3000;
+const PORT = 3000;
 server.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
